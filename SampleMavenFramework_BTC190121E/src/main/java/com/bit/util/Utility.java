5cf74571dbfe0478513a8f64c4d60efe747f1f5f
my name <you@examplejtge.com>
2019-04-03 19:22:31 -0400

    new project

---
+++ SampleMavenFramework_BTC190121E/src/main/java/com/bit/util/Utility.java
@@ -0,0 +1,10 @@
+package com.bit.util;
+
+public class Utility 
+{
+	public static void click()
+	{
+		System.out.println("clicked");
+	}
+
+}
---
+++ SampleMavenFramework_BTC190121E/src/test/java/com/bit/test/SmokeTest.java
@@ -0,0 +1,15 @@
+package com.bit.test;
+
+import org.junit.Test;
+
+import com.bit.util.Utility;
+
+public class SmokeTest 
+{
+	@Test
+	public void test1()
+	{
+		Utility.click();
+	}
+
+}
